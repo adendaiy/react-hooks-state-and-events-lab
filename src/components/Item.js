ab578572effffff12c79fdfa0630e41a4be5e00a
adendaiy <adenabdib@gmail.com>
2024-07-04 00:45:15 +0100

    this is my react state and event lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,34 @@
-import React from "react";
+// import React from "react";
+
+// function Item({ name, category }) {
+//   return (
+//     <li className="">
+//       <span>{name}</span>
+//       <span className="category">{category}</span>
+//       <button className="add">Add to Cart</button>
+//     </li>
+//   );
+// }
+
+// export default Item;
+
+
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+
+  const handleToggleCart = () => {
+    setInCart(!inCart); // Toggle between true and false
+  };
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleToggleCart}>
+        {inCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
